73f1c8e3307c2421412ce665a29cf340071481e5
rutep <peturda3@gmail.com>
2019-03-25 12:38:29 +0000

    Stats

---
+++ app/src/main/java/com/example/orkulindapp/SelectDateStatistics.java
@@ -0,0 +1,109 @@
+package com.example.orkulindapp;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class SelectDateStatistics extends AppCompatActivity {
+
+    EditText startDate, endDate;
+    TextView msg;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_date_statistics);
+
+        startDate = (EditText) findViewById(R.id.startDate);
+        endDate = (EditText) findViewById(R.id.endDate);
+        msg = (TextView) findViewById(R.id.msg);
+
+        Button buttonStartDate = (Button) findViewById(R.id.buttonStartDate);
+        Button buttonEndDate = (Button) findViewById(R.id.buttonEndDate);
+        Button buttonStats = (Button) findViewById(R.id.buttonStats);
+
+
+
+        buttonStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(SelectDateStatistics.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                startDate.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        buttonEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(SelectDateStatistics.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                endDate.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        buttonStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( startDate.getText().toString().length() > 0) {
+                    if(endDate.getText().toString().length() > 0) {
+                        Intent intent = new Intent(SelectDateStatistics.this, StatisticActivity.class);
+                        startActivity(intent);
+                    } else {
+
+                        msg.setText("Pleas input end date");
+                    }
+                } else {
+                    msg.setText("Pleas input start date");
+                }
+
+                
+            }
+        });
+
+
+
+    }
+
+}
